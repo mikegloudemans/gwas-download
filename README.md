00ac011f33436411a774b87f5282a3aced2b11e5
mikegloudemans <mgloud@durga.stanford.edu>
2018-05-24 16:15:15 -0700

    Added custom munging script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # gwas-download
 
+### Fast GWAS download script 
+
 This script downloads publicly available GWAS summary statistics from over 150 different publications.
 
 No software installation is necessary; the script runs directly in a Linux terminal:
@@ -15,6 +17,68 @@ Some of the files are freely accessible online, but must be downloaded manually 
 When using these data, please remember to acknowledge the authors of the original studies and to cite them in any publications. Links to each GWAS publication have been included in the download script.
 
 Help keep this list up to date by submitting pull requests with your own favorite studies. I’ll try to merge them in a timely manner.
+
+### GWAS munging scripts (updated 5/24)
+
+I've uploaded the `custom_munge.py` script that I used to convert these GWAS summary 
+statistics files into a standardized format. An output (formatted) GWAS summary 
+statistics file contains (at minimum) the rsids, chromosome numbers along with positions 
+in hg19, and p-values for every tested SNP. It will also retain the other columns 
+from the original file, appended to the end of the table in tab-separated format. 
+The output file will be sorted by chromosome and position, and indexed with tabix.
+
+The script requires a config file in JSON format with entries for each input GWAS file. 
+This requires a small amount of manual labor, but also takes care of a lot of common errors 
+that may occur in this process. Luckily, I’ve included _munge_menu.config_, which contains 
+the config specifications for the 200+ GWAS included in our original wget download script. 
+(Note that some of these files first need to be unzipped and may require other small formatting
+tasks before munging; thus, many but not all files will be ready for processing straight 
+after download with the wget script. I apologize for this limitation!) For each GWAS, 
+the config file requires the index of either 1) the rsid column or 2) the chromosome 
+number and SNP position columns. It also requires the index of the p-value column. 
+Other options exist for GWAS files with unusual formatting, and other columns such as 
+effect and noneffect alleles can be included as well. See the `munge_menu.config` file
+for a wide variety of examples.
+
+Running `custom_munge.py` will also require installation of the `tabix` command-line
+tool, as well as installation of the `pandas` library for Python. It also requires a
+local version of dbSNP version 150 formatted for use with hg19. You can download this with
+
+```
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/snp150.txt.gz
+```
+
+You'll then need to open the file `custom_munge.py` and navigate to the function
+`load_rsid_keys`, editing the line
+
+```
+    with gzip.open("/users/mgloud/projects/gwas/data/sorted_hg19_snp150.txt.gz") as f:
+```
+to show the location where you've stored dbSNP.
+
+You'll also need to open `munge_menu.config` and modify the lines
+
+```
+        "input_base_dir": "/mnt/lab_data/montgomery/shared/gwas/",
+        "output_base_dir": "/users/mgloud/projects/gwas/data/munged",
+```
+replacing these directories with the base directory where your input GWAS files are stored,
+and the base directory in which your formatted files should be stored.
+
+I apologize that this script is still pretty hackish. Please contact me or post a bug report if
+you encounter any issues that I may be able to help you resolve.
+
+While creating these scripts, I've found some other public tools for achieving very 
+similar tasks. In particular, check out [munge_sumstats.py](https://github.com/bulik/ldsc/blob/master/munge_sumstats.py)
+from the LD Score Regression toolbox. That script performs several QC steps and sanity checks 
+to make sure the formatted results it produces are trustworthy. I opted out of using that 
+tool because it automates some filtering steps where I prefer to have a bit more control, 
+which can lead to errors when using munge_sumstats.py on GWAS with unconventional formatting; 
+however, I find it excellent for running a first pass through a large number of GWAS files.
+
+If you know of any other tools/scripts for easily downloading and formatting GWAS, send me 
+a comment and I'll add a link. And feel free to repurpose or modify any of the code 
+here if it may be useful for you.
 
 ### Future Developments
 
---
+++ custom_munge.py
@@ -0,0 +1,284 @@
+#!/usr/bin/python
+# Author: Mike Gloudemans
+# Date created: 4/5/2018
+
+import json
+import pandas as pd
+import glob
+import gzip
+import sys
+import subprocess
+
+# Set debug to an integer if you only want to load a limited number of
+# rows from the input file, for debugging purposes.
+debug = None
+#debug = 1000000    
+
+# Custom script for munging all GWAS files, according to specifications
+# given in a separate JSON file.
+def main():
+
+    # Find location of config file and open it
+    if len(sys.argv) > 1:
+        munge_menu = sys.argv[1]
+    else:
+        munge_menu = "munge_menu.config"
+    with open(munge_menu) as f:
+        config = json.load(f)
+
+    rsids = load_rsid_keys()
+
+    # Munge every study from the config list, one at a time
+    for study in config["studies"]:
+        
+        print "Munging", study 
+
+        # Check if the config file specifies a custom delimiter
+        delimiter = "\t"
+        if "delimiter" in study:
+            delimiter = study["delimiter"]
+
+        # Check if we need to skip a certain number of rows
+        skip_rows = 0
+        if "skip_rows" in study:
+            skip_rows = int(study["skip_rows"])
+
+        # Parse each input trait separately, but output them
+        # all to the same final file.
+        first_trait = True
+        for trait in study["traits"]:
+            print "Current trait:", trait
+
+            # Some studies have several p-values for different traits, listed
+            # in the same file. For these ones, we need to do something slightly different
+            if "multi_column" in study:
+                file_chunks = study["multi_column"]
+                study["pvalue_index"] = study["traits"][trait][0]
+            else:
+                file_chunks = study["traits"][trait]
+
+            # Some files come in chunks separated by chromosome; loop through and combine all chunks.
+            all_data = []
+            for file_chunk in file_chunks:
+                filename = "/".join([config["input_base_dir"], study["study_info"], file_chunk])
+
+                # Determine format and load the file
+                if "format" in study:
+                    format = study["format"]
+                else:
+                    if filename.endswith(".gz"):
+                        format = "gzip"
+                    else:
+                        format = "txt"
+
+                if format == "gzip":
+                    with gzip.open(filename) as f:
+                        if "no_header" in study and study["no_header"] == "True":
+                            data = pd.read_csv(f, delimiter=delimiter, nrows=debug, skiprows = skip_rows, header=None)
+                        else:
+                            data = pd.read_csv(f, delimiter=delimiter, nrows=debug, skiprows = skip_rows)
+                else:
+                    if "no_header" in study and study["no_header"] == "True":
+                        data = pd.read_csv(filename, delimiter=delimiter, nrows=debug, skiprows = skip_rows, header=None)
+                    else:
+                        data = pd.read_csv(filename, delimiter=delimiter, nrows=debug, skiprows = skip_rows)
+
+                all_data.append(data)
+           
+            # Concatenate all the separate files for this trait
+            # into a single data frame.
+            data = pd.concat(all_data)
+
+            # Note key SNP attributes
+            if "effect_allele_index" in study:
+                data.rename(columns={data.keys()[int(study["effect_allele_index"]) - 1]:'effect_allele'}, inplace = True)
+            if "non_effect_allele_index" in study:
+                data.rename(columns={data.keys()[int(study["non_effect_allele_index"]) - 1]:'non_effect_allele'}, inplace = True)
+            if "direction_index" in study:
+                data.rename(columns={data.keys()[int(study["direction_index"]) - 1]:'effect_direction'}, inplace = True)
+
+                # Test if we're looking at "+/-"
+                if sum(data['effect_direction'].isin(["+", "-"])) * 1.0 / len(data['effect_direction']) > 0.9:
+                    # Leave things as they are
+                    pass
+
+                # Test if we're looking at odds ratios
+                elif sum(data['effect_direction'].astype(float) < 0) * 1.0 / len(data['effect_direction']) < 0.1:
+                    def sign(x):
+                        if x >= 1:
+                            return("+")
+                        else:
+                            return("-")
+                    data['effect_direction'] = data['effect_direction'].apply(sign)
+
+                # Otherwise, we're probably just looking at beta values
+                else:
+                    def sign(x):
+                        if x >= 0:
+                            return("+")
+                        else:
+                            return("-")
+                    data['effect_direction'] = data['effect_direction'].apply(sign)
+
+            if "rsid_index" in study and study["rsid_index"] != "-1":
+                # Join with rsid table to get indices for each column
+               
+                data.rename(columns={data.keys()[int(study["rsid_index"]) - 1]:'rsid'}, inplace = True)
+                data.rename(columns={data.keys()[int(study["pvalue_index"]) - 1]:'pvalue'}, inplace = True)
+
+                if "rsid_split" in study:
+                    def rsid_split(x):
+                        return x.split(study["rsid_split"]["splitter"])[int(study["rsid_split"]["index"])-1]
+
+                    data['rsid'] = data['rsid'].apply(rsid_split)
+                
+                # If there are multiple p-value columns, remove all of them except the one we're
+                # interested in
+                if "multi_column" in study:
+                    cols = data.columns.tolist()
+
+                    indices = [int(study["traits"][t][0])-1 for t in study["traits"] if t != trait]
+                    for index in sorted(indices, reverse=True):    
+                        del cols[index]
+                    data = data[cols]
+
+                new_data = rsids.merge(data, suffixes=('', '_old'), left_index=True, right_on="rsid")
+
+            elif "chr_index" in study and study["chr_index"] != "-1" \
+                    and "snp_pos_index" in study and study["snp_pos_index"] != "-1":
+
+                if study["chr_index"] == study["snp_pos_index"]:
+                    chrom = lambda x: x.split(study["snp_split_char"])[0]
+                    snp_pos = lambda x: x.split(study["snp_split_char"])[1]
+                    data["chr"] = data.iloc[:, int(study["chr_index"]) - 1].apply(chrom)
+                    data["snp_pos"] = data.iloc[:, int(study["chr_index"]) - 1].apply(snp_pos)
+                else:
+                    # Join with rsid table on chromosome and position
+                    data.rename(columns={data.keys()[int(study["chr_index"]) - 1]:'chr'}, inplace = True)
+                    data.rename(columns={data.keys()[int(study["snp_pos_index"]) - 1]:'snp_pos'}, inplace = True)
+                
+                data.rename(columns={data.keys()[int(study["pvalue_index"]) - 1]:'pvalue'}, inplace = True)
+
+                # If there are multiple p-value columns, remove all of them except the one we're
+                # interested in for this trait
+                if "multi_column" in study:
+                    cols = data.columns.tolist()
+
+                    indices = [int(study["traits"][t][0])+1 for t in study["traits"] if t != trait]
+                    for index in sorted(indices, reverse=True):
+                            del cols[index]
+                    data = data[cols]
+
+                data['chr'] = data['chr'].astype(str)
+                data['chr'] = data['chr'].str.replace('chr', '')
+                data['snp_pos'] = data['snp_pos'].astype(int)
+                
+                new_data = rsids.merge(data, suffixes=('', '_old'), on=["chr", "snp_pos"])
+
+            else:
+                print study["path_glob"], "not properly specified in JSON config file."
+                # TODO: print to a log file that the JSON was not properly
+                # specified for this file.
+                continue
+            
+            new_data = new_data[~(new_data['chr'].str.contains("_"))]
+            new_data = new_data[~(new_data['chr'].str.contains("NA"))]
+
+            # Add trait column if there are multiple traits in this study.
+            if len(study["traits"].keys()) > 1:
+                new_data['trait'] = trait
+
+            # Filter out rows that don't have valid pvals
+            def valid_pval(x):
+                try:
+                    y = float(x)
+                    if y < 0:
+                        return False
+                    if y > 1:
+                        return False
+                    return True
+                except:
+                    return False
+            new_data = new_data[new_data['pvalue'].apply(valid_pval)]
+
+            # Then reorder the new table appropriately
+
+            cols = new_data.columns.tolist()
+
+            cols.remove("rsid")
+            cols.remove("chr")
+            cols.remove("snp_pos")
+            cols.remove("pvalue")
+            if "trait" in cols:
+                cols.remove("trait")
+            if "rsid_old" in cols:
+                cols.remove("rsid_old")
+            if "effect_allele" in cols:
+                cols.remove("effect_allele")
+            if "non_effect_allele" in cols:
+                cols.remove("non_effect_allele")
+            if "effect_direction" in cols:
+                cols.remove("effect_direction")
+
+            prefix = []
+            if "effect_allele_index" in study:
+                prefix.append("effect_allele")
+            if "non_effect_allele_index" in study:
+                prefix.append("non_effect_allele")
+            if "direction_index" in study:
+                prefix.append("effect_direction")
+            cols = ["rsid", "chr", "snp_pos", "pvalue"] + prefix + cols
+            if len(study["traits"].keys()) > 1:
+                cols = ["trait"] + cols
+            new_data = new_data[cols]
+
+            print new_data.head(3)
+
+            # Write header only if it's the first trait from this study
+            if first_trait:
+                with open("tmp/unsorted_GWAS.tmp", "w") as w:
+                    new_data.to_csv(w, sep="\t", index=False, float_format='%.3E')
+            else:
+                with open("tmp/unsorted_GWAS.tmp", "a") as a:
+                    new_data.to_csv(a, sep="\t", index=False, header=False, float_format='%.3E')
+
+            first_trait = False
+
+        # Sort the new table and write it to its final destination file
+        if "output_file_name" in study:
+            # This is only used in cases where we want to output multiple files under a single
+            # study's directory. This would usually happen if the study contains
+            # input files with different formats.
+            out_file = "{0}/GWAS_{1}.txt".format(config["output_base_dir"], study["output_file_name"])
+        else:
+            out_file = "{0}/GWAS_{1}.txt".format(config["output_base_dir"], study["study_info"])
+        # TODO: This is unsafe. Fix it using Popen
+        subprocess.check_call("head -n 1 tmp/unsorted_GWAS.tmp > {0}".format(out_file), shell=True)
+        if len(study["traits"]) > 1:
+           subprocess.check_call("tail -n +2 tmp/unsorted_GWAS.tmp | sort -k3,3 -k4,4n >> {0}".format(out_file), shell=True) 
+        else:
+           subprocess.check_call("tail -n +2 tmp/unsorted_GWAS.tmp | sort -k2,2 -k3,3n >> {0}".format(out_file), shell=True) 
+
+        # Bgzip the output file
+        subprocess.check_call(["bgzip", "-f", out_file])
+
+        # Tabix the output file
+        if len(study["traits"]) > 1:
+            subprocess.check_call(["tabix", "-f", "-s", "3", "-b", "4", "-e", "4", "-S", "1", out_file+".gz"])
+        else:
+            subprocess.check_call(["tabix", "-f", "-s", "2", "-b", "3", "-e", "3", "-S", "1", out_file+".gz"])
+
+
+def load_rsid_keys():
+    with gzip.open("/users/mgloud/projects/gwas/data/sorted_hg19_snp150.txt.gz") as f:
+        data = pd.read_csv(f, sep="\t", index_col = 2, nrows=debug, header=None, names=["chr", "snp_pos"])
+
+    data['chr'] = data['chr'].astype(str)
+    data['snp_pos'] = data['snp_pos'].astype(int)
+    data['chr'] = data['chr'].str.replace('chr', '')
+    data['rsid'] = data.index.values
+
+    return data
+
+if __name__ == "__main__":
+    main()
